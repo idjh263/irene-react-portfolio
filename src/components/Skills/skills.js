ee417a885e9304ad81418fd4bb037694080ec484
idjh263 <idjh263@gmail.com>
2022-04-09 11:45:00 -0400

    added skills

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -3,12 +3,19 @@ import "./skills.css";
 import htmlicon from "../../assets/images/html.png";
 import cssicon from "../../assets/images/css.png";
 import jsicon from "../../assets/images/js.png";
+import npmicon from "../../assets/images/npm.png";
+import mongoicon from "../../assets/images/mongodb.png";
+import reacticon from "../../assets/images/react.png";
 
 const Skills = () => {
   return (
     <div>
-      <div className="skills">
-        <h2>SKILLS</h2>
+      <div className="skill-title">
+      <h2>SKILLS</h2>
+
+      </div>
+      <div className="skill-wrapper">
+       
 
         <div className="icons">
           <img src={htmlicon} alt="" />
@@ -19,8 +26,20 @@ const Skills = () => {
           <h3>CSS</h3>
         </div>
         <div className="icons">
-          <img src={jsicon} alt="css icon" />
+          <img src={jsicon} alt="js icon" />
           <h3>JS</h3>
+        </div>
+        <div className="icons">
+          <img src={npmicon} alt="npm icon" />
+          <h3>Node</h3>
+        </div>
+        <div className="icons">
+          <img src={mongoicon} alt="mongo icon" />
+          <h3>Mongo</h3>
+        </div>
+        <div className="icons">
+          <img src={reacticon} alt="react icon" />
+          <h3>React</h3>
         </div>
 
       </div>
